7205970bf0bc9c14702a21b20485800fe7fcd067
Robert Zenz <bobby@bonsaimind.org>
2015-03-07 18:43:16 +0100

    Added.

---
+++ print-functions.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+################################################################################
+#                                                                              #
+# Helper functions for centered printing of single-line and multi-line vars    #
+#                                                                              #
+# binaryanomaly - v0.2                                                         #
+# https://github.com/binaryanomaly/bash_files                                  #
+#                                                                              #
+################################################################################
+
+__print_line ()
+{
+    local line="--------------------------------------------------------------------------------"
+    
+    printf "${GREY}"
+    __print_centered_string "$line" "0"
+    printf "\n"
+}
+
+__print_ascii_art ()
+{
+    printf "%s\n" "$1" | figlet -w $COLUMNS
+}
+
+__print_ascii_art_lolcat ()
+{
+    { printf "%s\n" "$1" | figlet -w $COLUMNS -c ; __print_centered_string "$2" ; } | lolcat -S 550
+}
+
+
+__print_centered_string ()
+{
+    local c_string="$1"
+    local mid=$(((${#c_string}+$COLUMNS)/2))
+    local leftspace=$((${mid}-${#c_string}))
+
+    printf "%-*s%s\n" $leftspace " " "$c_string"
+}
+
+
+# Args: stringvar, offset
+__print_centered_multiline ()
+{
+    # Find longest string
+    local strlen=0
+    local offset=$2
+    
+    if [ -z "$offset" ]; then
+    	offset=0
+    fi
+    
+    while IFS= read -r line
+    do
+        # Store length
+        if [ ${#line} -gt $strlen ]; then
+            strlen="${#line}"
+        fi
+    done <<< "$1"
+
+    # Apply offset
+    strlen=$(($strlen-$offset))
+
+    # Print each line with offset of longest string
+    while IFS= read -r line
+    do
+        __print_fixed_singleline "$line" $strlen
+    done <<< "$1"
+
+    strlen=0
+}
+
+
+__print_fixed_singleline ()
+{
+    local c_string="$1"
+    local mid=$(((strlen+$COLUMNS)/2))
+    local leftspace=$((${mid}-strlen))
+    
+    printf "%-*s%s\n" $leftspace " " "$c_string"
+}
+
