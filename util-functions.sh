1d070d6b98a75a002e8171bf142b0acf0e91353c
Robert Zenz <Bobby@bonsaimind.org>
2015-03-18 00:15:37 +0100

    Added a few crude functions.

--- util-functions.sh
+++ util-functions.sh
@@ -16,3 +16,33 @@ __help() {
 	fi
 }
 
+__oneline() {
+	while read line; do
+		echo $line
+	
+		local remaining=$(($(tput cols)-${#line}))
+	
+		if [ $remaining -gt 0 ]; then
+			__cursor_up 1
+			local line=""
+			for counter in $(seq $remaining); do
+				line="$line "
+			done
+			echo $line
+		fi
+	
+		__cursor_up 1
+	done < /dev/stdin
+}
+
+__task_ok() {
+	__cursor_up 1
+	__cursor_forward 1
+	__echo_color $__tgreen -n OK
+	echo
+}
+
+__task_start() {
+	echo "[  ] $*"
+}
+
