44843647bd152654e6e92265b3d81814f5f62318
Robert Zenz <Bobby@bonsaimind.org>
2015-03-24 22:25:27 +0100

    Extracted df into its own function.

--- greet.sh
+++ greet.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
 
+__df_short_uuid() {
+	df --human-readable --exclude-type=tmpfs --exclude-type=devtmpfs --exclude-type=ecryptfs -T | \
+		tail --lines=+2 | \
+		sed -r "s/by-uuid\/([a-z0-9]+)\-([a-z0-9]+)\-([a-z0-9]+)\-([a-z0-9]+)\-([a-z0-9]+)/by-uuid\/...-\5/g" | \
+		sed -r "s/ {22}/            /g"
+}
+
 __greet() {
 	local hostname=$(hostname)
 	local kernelinfo=$(uname --kernel-name --kernel-release --hardware-platform --operating-system)
 	local date="$(date "+%a, %b %_d %Y · %I:%M:%S %p %z (%Z) · %s")"
-	local mounts="$(\
-		df --human-readable --exclude-type=tmpfs --exclude-type=devtmpfs --exclude-type=ecryptfs -T | \
-		tail --lines=+2 | \
-		sed -r "s/by-uuid\/([a-z0-9]+)\-([a-z0-9]+)\-([a-z0-9]+)\-([a-z0-9]+)\-([a-z0-9]+)/by-uuid\/...-\5/g" | \
-		sed -r "s/ {22}/            /g")"
+	local mounts="$(__df_short_uuid)"
 	local osinfo="$(lsb_release -ds) $(lsb_release -cs)"
 	local uptime="$(uptime)"
 	local users="$(who --users)"
